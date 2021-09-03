8d5af2984ffe260e7d97164251383605c1b1e633
F249 Master <f249master@champlaincollege.qc.ca>
2021-09-03 13:43:19 -0400

    Init commit

---
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        Model model;
+        public MainPage()
+        {
+            model = new Model();
+            InitializeComponent();
+            for(var i = 0; i < 10; i++)
+            {
+                Button btn = new Button();
+                btn.Text = i.ToString();
+                var j = i;
+                btn.Clicked += (e, s) => HandleNumClick(j);
+                CalcGrid.Children.Add(btn, i % 4, 3 + i / 4);
+            }
+
+        }
+
+        private void HandleNumClick(int num)
+        {
+            if(resultText.Text.Equals("0"))
+            {
+                resultText.Text = num.ToString();
+                return;
+            }
+            resultText.Text += num.ToString();
+        }
+
+        private void HandleEqualsClick(object sender, EventArgs e)
+        {
+            model.SecondNum = Convert.ToDecimal(resultText.Text); ;
+            resultText.Text = model?.Calculate().ToString() ?? "0";
+            model.Reset();
+        }
+
+        private void HandleAddition(object sender, EventArgs e)
+        {
+            model.FirstNum = Convert.ToDecimal(resultText.Text);
+            model.Op = Model.Operation.ADD;
+            resultText.Text = "0";
+        }
+
+        private void HandleClear(object sender, EventArgs e)
+        {
+            model.Reset();
+            resultText.Text = "0";
+        }
+    }
+}
---
+++ Calculator/Calculator/Model.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Model
+    {
+        public enum Operation
+        {
+            ADD,
+            SUBTRACT,
+            DIVIDE,
+            MULTIPLY,
+            SQUARE_ROOT,
+            PERCENTAGE
+        }
+
+        public decimal? FirstNum;
+        public decimal? SecondNum;
+        public decimal? Result;
+        public Operation? Op;
+
+        public void Reset()
+        {
+            FirstNum = SecondNum = null;
+            Op = null;
+            Result = null;
+        }
+
+        public decimal? Calculate()
+        {
+
+            if(FirstNum != null && SecondNum != null)
+            {
+                switch (Op) 
+                {
+                    case Operation.ADD:
+                        return FirstNum + SecondNum;
+                    case Operation.SUBTRACT:
+                        return FirstNum - SecondNum;
+                    case Operation.DIVIDE:
+                        return FirstNum / SecondNum;
+                    case Operation.MULTIPLY:
+                        return FirstNum * SecondNum;
+                }
+
+            }
+
+            return null;
+        }
+    }
+}
